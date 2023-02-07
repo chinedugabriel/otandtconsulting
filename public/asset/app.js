e2012d75654c3472cf2f76152a0a4f2534936ec3
chinedugabriel <chinedugabriel29@gmail.com>
2023-02-07 22:52:04 +0100

    added close and open function to toggle both menu

---
+++ public/asset/app.js
@@ -0,0 +1,39 @@
+// veriables for nav button
+let closeButtonNav = document.getElementById('close-nav');
+let openButtonNav = document.getElementById('open-nav');
+
+// nav section tobe toggled 
+let nav_list_section = document.getElementById('nav-list');
+let nav_wrapper_menu2 = document.getElementById('nav-wrapper');
+
+// green-slide-show 
+// let green_slide_show = document.getElementById('green-slide-show');
+
+function btnClose(){
+
+            // green_slide_show.style.display='none';
+
+            nav_list_section.style.display="none";
+
+    nav_wrapper_menu2.style.display='flex';
+    
+}
+
+function btnOpen(){
+    // setTimeout(()=>{
+
+    //         green_slide_show.style.display='none';
+    // },1000);
+
+    // green_slide_show.style.display='block';
+    
+    nav_wrapper_menu2.style.display='none';
+
+
+    nav_list_section.style.display="flex";
+}
+
+// events for navigation toggle
+
+closeButtonNav.addEventListener('click',btnClose);
+openButtonNav.addEventListener('click',btnOpen);
