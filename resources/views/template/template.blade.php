4330c4ede9ec9239e61e79a9ffd66665961d8f59
chinedugabriel <chinedugabriel29@gmail.com>
2023-02-07 07:50:02 +0100

    added the structure for nav

---
+++ resources/views/template/template.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="OT&T is a premier African information technology consulting firm. We analyse, develop, and implement solutions using technology and information systems for our customers."/><link rel="apple-touch-icon" href="favicon.png"/><link rel="manifest" href="manifest.json"/><link rel="stylesheet" href="css/webfont.css"/>
+    <title>OT&T</title>
+</head>
+<body>
+    
+</body>
+</html>
